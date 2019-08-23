c3199e34ed31bd9815c5315fab158639eae109c3
Aleksbgbg <kozichka01@abv.bg>
2019-08-23 15:21:21 +0100

    [Views/Index] Implement YouTube video player.

---
+++ Radio/wwwroot/js/main.js
@@ -0,0 +1,17 @@
+﻿function onYouTubeIframeAPIReady() {
+    new YT.Player("player",
+        {
+            width: "100%",
+            height: "100%",
+            videoId: "SsQM3c4KPW4",
+            playerVars: {
+                autoplay: 1,
+                controls: 0,
+                disablekb: 1,
+                fs: 0,
+                iv_load_policy: 3
+            }
+        });
+
+    onYouTubeIframeAPIReady = undefined;
+}
