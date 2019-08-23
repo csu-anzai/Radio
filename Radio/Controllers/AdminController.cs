320569b122b1dcd34aeec980792de10c11077cf5
Aleksbgbg <kozichka01@abv.bg>
2019-08-23 18:39:44 +0100

    [AdminController] Add admin login.

---
+++ Radio/Controllers/AdminController.cs
@@ -0,0 +1,57 @@
+﻿namespace Radio.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Radio.Models;
+
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AdminController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet("/Login")]
+        public ViewResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost("/Login")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Login login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            AppUser targetUser = await _userManager.FindByNameAsync(login.Username);
+
+            if (targetUser == null)
+            {
+                ModelState.AddModelError(nameof(login.Username), "Username does not exist.");
+                return View();
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(targetUser, login.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (signInResult.Succeeded)
+            {
+                return Redirect(returnUrl ?? "/admin");
+            }
+
+            ModelState.AddModelError(nameof(login.Password), "Incorrect password.");
+            return View();
+        }
+    }
+}
--- Radio/Startup.cs
+++ Radio/Startup.cs
@@ -2,16 +2,38 @@ namespace Radio
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
 
     using Radio.Hubs;
+    using Radio.Models;
+    using Radio.Models.Database;
     using Radio.Services;
 
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseNpgsql(_configuration["Data:ConnectionString"]));
+
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+                    {
+                        options.User.AllowedUserNameCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-._@/ ";
+                        options.Password.RequireLowercase = false;
+                    })
+                    .AddEntityFrameworkStores<AppIdentityDbContext>()
+                    .AddDefaultTokenProviders();
+
             services.AddMvc();
             services.AddSignalR();
 
@@ -26,6 +48,8 @@ namespace Radio
             }
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseRouting();
             app.UseEndpoints(endpoints =>
