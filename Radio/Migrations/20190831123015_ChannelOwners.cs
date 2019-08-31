aa8d765074b2356fceb0cdce3a9b9c2d2c6ee396
Aleksbgbg <kozichka01@abv.bg>
2019-08-31 13:45:14 +0100

    [Models] Add user owners to channels.

---
+++ Radio/Migrations/20190831123015_ChannelOwners.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Radio.Migrations
+{
+    public partial class ChannelOwners : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Channels",
+                nullable: false,
+                defaultValue: "ba15be3a-68a1-4a99-9eb6-f3262658042b"); // Requires default owner ID - use admin's ID to give ownership of all channels
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Channels_OwnerId",
+                table: "Channels",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Channels_AspNetUsers_OwnerId",
+                table: "Channels",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Channels_AspNetUsers_OwnerId",
+                table: "Channels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Channels_OwnerId",
+                table: "Channels");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Channels");
+        }
+    }
+}
--- Radio/Models/Channel.cs
+++ Radio/Models/Channel.cs
@@ -2,6 +2,9 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using Radio.Models.User;
 
     public class Channel
     {
@@ -11,6 +14,12 @@
         public string Name { get; set; }
 
         public ushort Discriminator { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Owner))]
+        public string OwnerId { get; set; }
+
+        public AppUser Owner { get; set; }
 
         public ICollection<ChannelTrack> ChannelTracks { get; set; }
     }
